a9136c87f321092ff12b870cf942adaf19e3f218
Julien Duclos <105755221+duclosjulien@users.noreply.github.com>
2026-05-19 21:30:08 -0400

    Changed the message mapping from chat.sendMessage to chat.message

--- src/main/java/com/ravyn/chat/chat/ChatController.java
+++ src/main/java/com/ravyn/chat/chat/ChatController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-    @MessageMapping("/chat.sendMessage")
+    @MessageMapping("/chat.send")
     @SendTo("/topic/public")
 
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -60,7 +60,7 @@ function sendMessage(event) {
             content: messageInput.value,
             type: 'CHAT'
         };
-        stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
+        stompClient.send("/app/chat.send", {}, JSON.stringify(chatMessage));
         messageInput.value = '';
     }
     event.preventDefault();
