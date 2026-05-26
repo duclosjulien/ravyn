98732e399e4acf3360bde72d2ceb3a38bbf39a53
Julien Duclos <105755221+duclosjulien@users.noreply.github.com>
2026-05-26 18:32:47 -0400

    Update api.ts

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,4 +1,4 @@
-import { User } from './types';
+import { User, Conversation } from './types';
 
 export async function userLogin(username: string): Promise<User> {
     const response = await fetch("/users/login", {
@@ -8,4 +8,12 @@ export async function userLogin(username: string): Promise<User> {
     });
     const user = await response.json();
     return user as User;
+}
+
+export async function createConversation(user1Id: number, user2Id: number): Promise<Conversation> {
+    const response = await fetch("conversations/create", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ username: username })
+    }
 }
