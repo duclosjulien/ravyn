70dcaae08ad7342dc50a91a30346f9443c7f8ade
Julien Duclos <105755221+duclosjulien@users.noreply.github.com>
2026-06-01 18:01:29 -0400

    Stop tracking compiled TypeScript output

--- src/main/resources/static/js/api.d.ts
+++
@@ -1,6 +0,0 @@
-import { User, Conversation, UserSummary } from './types';
-export declare function userLogin(username: string): Promise<User>;
-export declare function createConversation(user1Id: number, user2Id: number): Promise<Conversation>;
-export declare function getConversationsByUserId(userId: number): Promise<Conversation[]>;
-export declare function findUserByUsername(username: string): Promise<UserSummary | null>;
-//# sourceMappingURL=api.d.ts.map
