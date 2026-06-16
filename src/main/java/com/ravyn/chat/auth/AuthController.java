67659f06b7d6b3a4f981108c19af3b09d570c0dc
Julien Duclos <105755221+duclosjulien@users.noreply.github.com>
2026-06-16 01:42:24 -0400

    Modified the login/register/chat pages
    Currently working on auth

---
+++ src/main/java/com/ravyn/chat/auth/AuthController.java
@@ -0,0 +1,13 @@
+package com.ravyn.chat.auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/me")
+
+}
