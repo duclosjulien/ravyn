b99bed88b678549a5902698c9d91209fbad864f5
Julien Duclos <105755221+duclosjulien@users.noreply.github.com>
2026-08-19 00:40:04 -0400

    chore: upgrade mobile app to Expo SDK 57

--- mobile/app/index.d.ts
+++
@@ -1,2 +0,0 @@
-export default function ChatPrototypeScreen(): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=index.d.ts.map
--- mobile/src/components/ConversationList.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConversationPreview } from "@/types/chat";
-interface ConversationListProps {
-    conversations: ConversationPreview[];
-    activeConversationId?: string;
-    onSelectConversation: (conversationId: string) => void;
-}
-export declare function ConversationList({ conversations, activeConversationId, onSelectConversation }: ConversationListProps): import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=ConversationList.d.ts.map
